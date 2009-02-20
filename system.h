248e91025613d6f741cfbd943f10c5d95619dd6a
Charlie Shepherd <masterdriverz@gmail.com>
2009-02-20 14:20:59 +0000

    Style changes to screen.c

--- system.h
+++ system.h
@@ -5,3 +5,11 @@ int strcmp(const char *s1, const char *s2);
 int strncmp(const char *s1, const char *s2, size_t n);
 char *strcpy(char *dest, const char *src);
 char *strncpy(char *dest, const char *src, size_t n);
+
+void scroll(void);
+void move_csr(void);
+void cls(void);
+void putch(unsigned char c);
+void puts(unsigned char *text);
+void settextcolor(unsigned char forecolor, unsigned char backcolor);
+void init_video(void);
