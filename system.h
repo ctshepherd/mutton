001254223884a038b09cdde2960a344c4bd5bc03
Charlie Shepherd <masterdriverz@gmail.com>
2009-02-26 17:00:39 +0000

    Tidy up system.h a little

--- system.h
+++ system.h
@@ -5,12 +5,15 @@ void idt_install(void);
 void isrs_install(void);
 
 struct regs {
-	unsigned int gs, fs, es, ds;      /* pushed the segs last */
-	unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;  /* pushed by 'pusha' */
-	unsigned int int_no, err_code;    /* our 'push byte #' and ecodes do this */
-	unsigned int eip, cs, eflags, useresp, ss;   /* pushed by the processor automatically */
+	/* pushed the segs last */
+	unsigned int gs, fs, es, ds;
+	/* pushed by 'pusha' */
+	unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;
+	/* our 'push byte #' and ecodes do this */
+	unsigned int int_no, err_code;
+	/* pushed by the processor automatically */
+	unsigned int eip, cs, eflags, useresp, ss;
 };
 
 void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel,
 				unsigned char flags);
-
