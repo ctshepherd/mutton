bfdec063bbfc54625f5c53881ec82315fe74c1df
Charlie Shepherd <masterdriverz@gmail.com>
2009-02-26 18:42:46 +0000

    Rewrite comment a bit more succintly

--- idt.c
+++ idt.c
@@ -15,12 +15,10 @@ struct idt_ptr {
 	unsigned int base;
 } __attribute__((packed));
 
-/* Declare an IDT of 256 entries. Although we will only use the
-*  first 32 entries in this tutorial, the rest exists as a bit
-*  of a trap. If any undefined IDT entry is hit, it normally
-*  will cause an "Unhandled Interrupt" exception. Any descriptor
-*  for which the 'presence' bit is cleared (0) will generate an
-*  "Unhandled Interrupt" exception */
+/* Declare an IDT of 256 entries. Unused entries exist as a trap - if any
+ * undefined IDT entry is hit, it normally will cause an "Unhandled Interrupt"
+ * exception: Any descriptor for which the 'presence' bit is cleared (0)
+ * generates an "Unhandled Interrupt" exception */
 static struct idt_entry idt[256];
 struct idt_ptr idtp;
 
