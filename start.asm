70eb80e1b35d4ba5762c7e4ad504af0657af899b
Charlie Shepherd <masterdriverz@gmail.com>
2009-03-23 20:27:52 +0000

    Remove multiboot kludge, allows us to build an ELF executable

--- start.asm
+++ start.asm
@@ -6,14 +6,13 @@ start:
     mov esp, _sys_stack     ; This points the stack to our new stack area
     jmp stublet
 
-; This part MUST be 4byte aligned, so we solve that issue using 'ALIGN 4'
+; This part MUST be 4byte aligned
 ALIGN 4
 mboot:
     MULTIBOOT_PAGE_ALIGN	equ 1<<0
     MULTIBOOT_MEMORY_INFO	equ 1<<1
-    MULTIBOOT_AOUT_KLUDGE	equ 1<<16
     MULTIBOOT_HEADER_MAGIC	equ 0x1BADB002
-    MULTIBOOT_HEADER_FLAGS	equ MULTIBOOT_PAGE_ALIGN | MULTIBOOT_MEMORY_INFO | MULTIBOOT_AOUT_KLUDGE
+    MULTIBOOT_HEADER_FLAGS	equ MULTIBOOT_PAGE_ALIGN | MULTIBOOT_MEMORY_INFO
     MULTIBOOT_CHECKSUM	equ -(MULTIBOOT_HEADER_MAGIC + MULTIBOOT_HEADER_FLAGS)
     EXTERN code, bss, end
 
@@ -21,13 +20,6 @@ mboot:
     dd MULTIBOOT_HEADER_MAGIC
     dd MULTIBOOT_HEADER_FLAGS
     dd MULTIBOOT_CHECKSUM
-
-    ; AOUT kludge - must be physical addresses.
-    dd mboot
-    dd code
-    dd bss
-    dd end
-    dd start
 
 SECTION .data
 
