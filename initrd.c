6abcd850edf0c5d31e220de460ccf668a6e8bb6e
Charlie Shepherd <masterdriverz@gmail.com>
2009-04-09 22:43:33 +0100

    Fix initrd format checking, don't memcpy the remainder of the disk and put out some informative error messages

--- initrd.c
+++ initrd.c
@@ -5,6 +5,7 @@
  */
 
 #include "vfs.h"
+#include "screen.h"
 #include "string.h"
 #include "system.h"
 #include "malloc.h"
@@ -34,7 +35,7 @@ struct vfs_inode *initrd_open(struct superblock *s, uint32_t inode)
 	vi->mask = di->mask;
 	vi->uid = di->uid;
 	vi->gid = di->gid;
-	vi->flags = di->flags;
+	vi->flags = RDONLY;
 	vi->ino = di->ino;
 	vi->length = di->length;
 	vi->super = s;
@@ -59,55 +60,56 @@ unsigned init_initrd_fs(struct superblock *s, char *disk, size_t length)
 	struct initrd_superblock *sb;
 	struct vfs_dirent *d;
 	struct initrd_inode *i;
+	uint32_t *header;
 	uint32_t version, checksum;
-
-	s->fs = &initrd_fs;
 
 	sb = malloc(sizeof(struct initrd_superblock));
 	if (!sb)
 		return 1;
 
-	if (length < 2*4+sizeof(struct vfs_dirent)) {
+	if (length < 2*4) {
 		free(sb);
 		return 2;
 	}
-	length -= 2*4+sizeof(struct vfs_dirent);
+	length -= (2*4);
 
-	version = *(uint32_t *)disk++;
-	checksum = *(uint32_t *)disk++;
+	header = (uint32_t *)disk;
+	version = *header++;
+	checksum = *header++;
 	if ((version^checksum) != INITRD_MAGIC) {
 		free(sb);
+		puts("initrd: could not load disk: invalid format\n");
 		return 2;
 	}
 	if (version != SUPPORTED_VERSION) {
 		free(sb);
+		puts("initrd: could not load disk: wrong version\n");
 		return 2;
 	}
 
-	d = sb->file_list = (struct vfs_dirent *)disk;
+	d = sb->file_list = (struct vfs_dirent *)header;
 	do {
 		if (length < sizeof(struct vfs_dirent)) {
 			free(sb);
+			puts("initrd: could not load disk: wrong length (dirent)\n" );
 			return 2;
 		}
 		length -= sizeof(struct vfs_dirent);
-	} while (d->name && d++);
+	} while (*d->name && d++);
 
 	i = sb->inode_list = (struct initrd_inode *)d;
 	do {
 		if (length < sizeof(struct initrd_inode)) {
 			free(sb);
+			puts("initrd: could not load disk: wrong length (inode)\n");
 			return 2;
 		}
 		length -= sizeof(struct initrd_inode);
 	} while (i->flags && i++);
 
-	sb->contents = malloc(length);
-	if (!sb->contents) {
-		free(sb);
-		return 1;
-	}
-	memcpy(sb->contents, (char *)i, length);
+	sb->contents = (char *)i;
+	s->fs = &initrd_fs;
+	s->priv = sb;
 
 	return 0;
 }
