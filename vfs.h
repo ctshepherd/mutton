75e3409fef53907ca63c4e9cf9e2ddadb9bcfad3
Charlie Shepherd <masterdriverz@gmail.com>
2009-04-10 22:19:45 +0100

    Update typedefs to reflect new vfs wrapper declarations

--- vfs.h
+++ vfs.h
@@ -35,9 +35,9 @@ struct vfs_inode {
 
 typedef unsigned (*read_type_t)(struct vfs_inode *f, unsigned offset, unsigned size, char *buf);
 typedef unsigned (*write_type_t)(struct vfs_inode *f, unsigned offset, unsigned size, char *buf);
-typedef struct vfs_inode *(*open_type_t)(struct superblock *s, uint32_t inode);
+typedef unsigned (*open_type_t)(struct superblock *s, uint32_t inode, struct vfs_inode *f);
 typedef void (*delinode_type_t)(struct vfs_inode *f);
-typedef struct vfs_dirent *(*readdir_type_t)(struct vfs_inode *f, unsigned index);
+typedef unsigned (*readdir_type_t)(struct vfs_inode *f, unsigned index, struct vfs_dirent *d);
 typedef struct vfs_inode *(*finddir_type_t)(struct vfs_inode *f, char *name);
 typedef unsigned (*initfs_type_t)(struct superblock *s, char *disk, size_t length);
 
